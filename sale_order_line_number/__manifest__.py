128c1d305d9770de8a1a32f25f54534b933734f5
Christodoula <christodoula@eplanconsultants.com>
2021-07-19 13:51:42 +0300

    Custom Module: Sale Order Line Number

---
+++ sale_order_line_number/__manifest__.py
@@ -0,0 +1,22 @@
+
+{
+    'name': 'A/A Sale Order Line',
+    'version': '1.0',
+    'category': 'Sales',
+    'sequence': 4,
+    'author': 'Eplan',
+    'website': 'https://www.eplanconsultants.com/',
+    'images': [
+    ],
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_order_views.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
